fbb3e4e78071325d85043510d1175cf7cadc8d15
Panchueco117 <100866288+Panchueco117@users.noreply.github.com>
2026-02-05 12:35:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const path = require("path");
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 app.use(express.static(__dirname));
 
 app.use("/img", express.static("img"));
@@ -257,6 +258,7 @@ app.post(
         }
     }
 );
+
 
 
 
