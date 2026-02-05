55d9b960930e0f0ded4da35274035c101b3bd021
Panchueco117 <100866288+Panchueco117@users.noreply.github.com>
2026-02-05 12:30:46 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,14 @@ const path = require("path");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.static(__dirname));
 
 app.use("/img", express.static("img"));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
 /* =========================
    CONFIGURACIÓN DIRECTA
 ========================= */
@@ -252,6 +257,7 @@ app.post(
         }
     }
 );
+
 
 
 
